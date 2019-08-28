acf262b448097242fe3ab2440b66c68561c68e88
siko408 <gamerbloxhd@gmail.com>
2019-08-28 01:09:33 -0700

    static website pages

---
+++ character_resume.js
@@ -0,0 +1,14 @@
+
+var sign = "<"
+sign.fontsize(10);
+document.getElementById("leftArrowbtn").innerHTML = sign;
+function pageChange(){
+	//console.log("button clicked");
+	//var x = document.createElement("A");
+	//x.setAttribute("href","character_resume_2.html");
+	//document.getElementById("button2").appendChild(x);
+	location.href = "character_resume_2.html";
+}
+function mainPage(){
+	location.href = "character_resume.html";
+}
