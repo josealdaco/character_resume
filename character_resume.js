e22f65f77c7b6ef03d7994b3462e486c80fbc4da
siko408 <gamerbloxhd@gmail.com>
2019-09-02 19:23:07 -0700

    Fixed bugs

--- character_resume.js
+++ character_resume.js
@@ -1,14 +1,56 @@
 
 var sign = "<"
 sign.fontsize(10);
-document.getElementById("leftArrowbtn").innerHTML = sign;
+document.getElementById("leftArrowbtn").innerHTML = sign; 
+/*String.prototype.format = function(){ /// stackoverflow, Make sure to study the functionality
+	var i = 0, args = arguments;
+	return this.replace(\{}\g, function(){
+	return typeof(args[i] != 'undefined' ? args[i++]:'')
+						})
+}
+*/
+
+/// temperary error
 function pageChange(){
-	//console.log("button clicked");
-	//var x = document.createElement("A");
-	//x.setAttribute("href","character_resume_2.html");
-	//document.getElementById("button2").appendChild(x);
+	
 	location.href = "character_resume_2.html";
 }
 function mainPage(){
 	location.href = "character_resume.html";
+}
+function math_libs(){
+	console.log("clicked");
+	location.href = "math_libs.html";
+}
+function submit(){
+	noun = document.getElementById("noun").value;
+	adjective = document.getElementById("adjective").value;
+	verb = document.getElementById("verb").value;
+	console.log(noun);
+	console.log(adjective);
+	console.log(verb);
+	///Check for string logic here
+	
+	
+	
+	if(noun == "" || adjective == "" || verb == "")
+		{
+			document.getElementById("error").innerHTML = "ONE OR MORE BOXES WERE LEFT BLANK"
+			document.getElementById("response").innerHTML = ""
+		}
+	else{
+
+	document.getElementById("response").innerHTML =  "TODAY WAS ONE OF THE BRIGHEST" + " " +  noun + " " + "IN MY LIFETIME. NEVER WOULD HAVE IMAGINED SUCH" + " " + adjective + " " + "WHILE I WAS" + " " + verb + ".";
+			document.getElementById("noun").value = "";
+	document.getElementById("adjective").value = "";
+	document.getElementById("verb").value = "";
+		document.getElementById("error").innerHTML = ""
+	}
+	
+	
+	
+	
+
+
+
 }
